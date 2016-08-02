cd4faadfacdbd2aed372d9a9abe99395921646c0
Joshua Whetton <whetton.josh@gmail.com>
2016-08-01 22:35:52 -0400

    Adjusted Topic and updated gitignore to avoid tracking egg/build info.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Code Generation',
+        'Topic :: Text Processing',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
